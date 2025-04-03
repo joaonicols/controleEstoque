840da0fb7146e343a626f3ce602c1d8c6d792166
joão <joaonsarruda@gmail.com>
2025-04-03 10:24:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// INFORMAÇÕES DO FORMULÁRIO
+const nome = document.getElementById("nome");
+
+const categoria = document.getElementById("categoria");
+
+const preco = document.getElementById("preco");
+
+const quantidade = document.getElementById("quantidade");
+
+const imagem = document.getElementById("imagem");

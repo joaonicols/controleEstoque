18f99c2d39cb6cbb68cf92464474bfb61d6360ee
joão <joaonsarruda@gmail.com>
2025-04-03 11:02:54 -0300

    second commit

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 // INFORMAÇÕES DO FORMULÁRIO
 const nome = document.getElementById("nome");
-
 const categoria = document.getElementById("categoria");
-
 const preco = document.getElementById("preco");
-
 const quantidade = document.getElementById("quantidade");
-
 const imagem = document.getElementById("imagem");
+const produtoForm = document.getElementById("produto-form");
+
+
+produtoForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if(nome.value == ''){
+        document.getElementById("erro-nome").style.display = 'block';
+    }else{
+        document.getElementById("erro-nome").style.display = 'none';
+    }
+});
+
